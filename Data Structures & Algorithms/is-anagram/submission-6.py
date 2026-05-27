e8dcc57d7fbb02a97a979bccd68666455fcfc19f
Aditya dimble <102818268+Adidimble@users.noreply.github.com>
2026-05-27 10:12:40 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+        map1 = {}
+        map2 = {}
+
+        for i in range(len(s)):
+            map1[s[i]] =  map1.get(s[i],0) + 1
+            map2[t[i]] =  map2.get(t[i],0) + 1
+
+        if map1!=map2:
+            return False
+
+        return True
