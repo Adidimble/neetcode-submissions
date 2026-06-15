85827be826aaa377c5107ef3e1a385bdeab90c51
Aditya dimble <102818268+Adidimble@users.noreply.github.com>
2026-06-15 19:46:11 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,14 @@
+import sys
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        mini = sys.maxsize
+        #check the profit for every price and use the left most mini (the lowest selling price) to calculate the profit
+        for i in prices:
+            if mini>i:
+                mini = i
+            local_profit = i-mini
+            if local_profit> profit:
+                profit = local_profit
+        
+        return(profit)
